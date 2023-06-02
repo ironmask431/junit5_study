24a7947f547588342fe0b1f9e2ee6b6ad724cec7
ironmask431 <ironmask431@gmail.com>
2023-06-02 17:31:56 +0900

    Initial commit

---
+++ src/test/java/kevin/junit5_study/Junit5StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package kevin.junit5_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
