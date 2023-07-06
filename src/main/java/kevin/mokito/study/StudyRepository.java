285fb97e20a24c816041b903a2e7dc56f44f5035
ironmask431 <ironmask431@gmail.com>
2023-07-06 17:57:46 +0900

    mokito 진행중

--- src/main/java/kevin/mokito/study/StudyRepository.java
+++ src/main/java/kevin/mokito/study/StudyRepository.java
@@ -2,7 +2,8 @@ package kevin.mokito.study;
 
 
 import kevin.mokito.domain.Study;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudyRepository {
+public interface StudyRepository extends JpaRepository<Study, Long> {
     Study save(Study study);
 }
--- src/test/java/kevin/mokito/study/StudyServiceTest.java
+++ src/test/java/kevin/mokito/study/StudyServiceTest.java
@@ -1,7 +1,24 @@
 package kevin.mokito.study;
 
+import kevin.mokito.member.MemberService;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class StudyServiceTest {
 
+    @Test
+    void createStudyService(){
+
+        //mock을 이용해 임의로 인터페이스의 구현체를 만들 수 있다.
+        MemberService memberService = mock(MemberService.class);
+        StudyRepository studyRepository = mock(StudyRepository.class);
+
+        //StudyService 를 생성하는데 필요한 MemberService 와 StudyRepository 가 구현체가 없는 인터페이스이므로 생성할 수가 없다.
+        //이런 경우 mocking 을 해서 생성 할 수있다.
+        StudyService studyService = new StudyService(memberService, studyRepository);
+
+        assertNotNull(studyService);
+    }
 }
