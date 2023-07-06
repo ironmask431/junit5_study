328888d2558063408711de2bb73a4bf1341524f4
ironmask431 <ironmask431@gmail.com>
2023-07-07 08:52:45 +0900

    mock 객체 stubbing

--- src/test/java/kevin/mokito/study/StudyServiceTest.java
+++ src/test/java/kevin/mokito/study/StudyServiceTest.java
@@ -22,11 +22,11 @@ class StudyServiceTest {
     @Test
     void createStudyService(){
 
-        //방법1. mock을 이용해 임의로 인터페이스의 구현체를 만들 수 있다.
+        //방법1. mock을 이용해 임의로 인터페이스의 가상 구현체를 만들 수 있다.
 //        MemberService memberService = mock(MemberService.class);
 //        StudyRepository studyRepository = mock(StudyRepository.class);
 
-        //StudyService 를 생성하는데 필요한 MemberService 와 StudyRepository 가 구현체가 없는 인터페이스이므로 생성할 수가 없다.
+        //StudyService 를 생성하는데 필요한 MemberService 와 StudyRepository 인터페이스 가 구현체가 없으므로 studyService 객체를 생성할 수가 없다.
         //이런 경우 mocking 을 해서 생성 할 수있다.
         StudyService studyService = new StudyService(memberService, studyRepository);
 
