eed9e628265523166e1428a70fac0aec68a67797
ironmask431 <ironmask431@gmail.com>
2023-07-19 12:47:04 +0900

    test_container 소개

--- src/test/java/kevin/test_container/TestContainerTest.java
+++ src/test/java/kevin/test_container/TestContainerTest.java
@@ -3,6 +3,11 @@ package kevin.test_container;
 /**
  * test_container 소개
  * test_container는 실습생략. 강의만 들음.
+ *
+ * 테스트에서 도커 컨테이너를 실행 할 수 있는 라이브러리.
+ * 테스트 실행 시 DB를 설정하거나 별도의 프로그램 또는 스크립트를 실행할 필요 없다.
+ * 보다 production 에 가까운 테스트를 만들 수 있다.
+ * 테스트가 느려진다.
  */
 public class TestContainerTest {
 }
